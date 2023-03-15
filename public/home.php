7e7af48fd02c2afc02455dd571fe83db4c0156e7
DominusDrow <galacticovasquez@gmail.com>
2023-03-14 19:54:48 -0600

    header

---
+++ public/home.php
@@ -0,0 +1,34 @@
+
+<?php include("./outdoorealestate/includes/header.php") ?>
+
+<?php include("./outdoorealestate/navbar.php") ?>
+
+
+
+<div class="bg-cover bg-center h-50" style="background-image: url('./outdoorealestate/src/imgBackgroud.jpg');">
+  <div class="flex flex-col justify-center items-center h-full text-white">
+    <h1 class="text-3xl lg:text-4xl font-bold mt-20">Wes & Keri Reidhead - </h1>
+    <h1 class="text-3xl lg:text-4xl font-bold">OutdooRealestate@West USA Realty</h1>
+    <h2 class="text-lg lg:text-xl font-medium mb-10 mt-3">Find Your Dream Home Now</h2>
+    <div class="w-full max-w-sm">
+      <form>
+        <div class="flex items-center border-b border-b-2 border-gray-300 py-2">
+          <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search for address, city, country or zipcode">
+        </div>
+        <div class="text-center">
+          <button class="flex-shrink-0 bg-transparent text-white py-1 px-2 rounded border mt-10 mb-20 hover:bg-gray-500" type="button">
+            Advanced Search
+          </button>
+        </div>
+          
+
+      </form>
+    </div>
+  </div>
+</div>
+
+
+
+
+</body>
+</html>
